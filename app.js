3f452c79dec7b3dfbe210a1bd8588d6cbb2cf14b
Nenad <nstokic@gmail.com>
2019-01-30 12:58:56 +0100

    before css styling

--- app.js
+++ app.js
@@ -2,12 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const moment = require("moment");
-
-// const now = moment();
-// const day = now.format("dddd");
-// const currentDay = now.day();
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:"true"}));
@@ -26,19 +20,16 @@ app.get("/", function(req, res){
 
   var day = today.toLocaleDateString("sr", options);
 
-  // if(currentDay === 6 || currentDay === 0){
-  // //  day = "Weekend";
-  // } else {
-  // //  day = "Weekday";
-  // }
-
-  res.render("list", {kindOfDay: day});
+  res.render("list", {
+    kindOfDay: day
+  });
 
 });
 
 app.post("/", function(req, res){
-  var input = req.body.newItem;
-    console.log(input);
+  var item = req.body.newItem;
+
+  res.render("list", {newListItem: item});
 });
 
 app.listen(3000, function(){
