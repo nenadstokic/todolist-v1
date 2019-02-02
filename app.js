367c62cd2c510c3ddf75332dfff5f88a8426c380
Nenad <nstokic@gmail.com>
2019-02-02 01:38:46 +0100

    Finished todolist v1 (lecture 237)

--- app.js
+++ app.js
@@ -2,37 +2,46 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const app = express();
 
-let items= ["Buy food", "Cook food", "Eat food"];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:"true"}));
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
-
 app.get("/", function(req, res){
 
-  let today = new Date();
+  const day = date.getDate();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("sr", options);
-
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 
 });
 
 app.post("/", function(req, res){
-  let item = req.body.newItem;
 
-  items.push(item);
+  const item = req.body.newItem;
 
-  res.redirect("/");
+console.log(req.body);
+
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req,res){
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
 });
 
 app.listen(3000, function(){
